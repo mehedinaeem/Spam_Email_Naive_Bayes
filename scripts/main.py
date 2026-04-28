3f647eab48c087b4ed982fb5e67dd838a7ca99f5
mehedinaeem <mehedinaeem00@gmail.com>
2026-04-28 16:11:20 +0600

    EDA

---
+++ scripts/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parents[1]
+df = pd.read_csv(ROOT_DIR / "data" / "emails.csv")
+
+# Check some spam samples
+print(df[df["spam"] == 1]["text"].head(10))
+
+# Check some non-spam samples
+print(df[df["spam"] == 0]["text"].head(10))
